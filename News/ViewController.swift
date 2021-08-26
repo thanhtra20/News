e2634bfd3d7266cca8ce77dccbb37a130b9207c1
thanhtra29 <1801040222@s.hanu.edu.vn>
2021-08-26 21:28:13 +0700

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Nguyễn Trà on 8/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
